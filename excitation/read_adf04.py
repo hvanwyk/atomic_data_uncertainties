9fd8c1e9fb75edae830da6c6b9d9b588e5b810d6
Kyle Stewart <kyle4@vt.edu>
2019-09-25 16:27:12 -0400

    comparing rates only at relevant temperatures

--- excitation/read_adf04.py
+++ excitation/read_adf04.py
@@ -93,14 +93,14 @@ def read_adf04_np(filename):
     T = np.array(T)
     levels = pd.DataFrame({"#": levels[:, 0], "config": levels[:, 1], "(2S+1)L( 2J)": levels[:, 2], "Energy": levels[:, 3]})
 
-    return levels, T.astype(np.float), rates.astype(np.float)
+    return levels, T.astype(np.float)[2:], rates.astype(np.float)
 
 def rates_dataframe(filename):
     
     levels, T, rates = read_adf04_np(filename)
     
     dct = {"initial": rates[:, 0], "final": rates[:, 1]}
-    for i in range(2, rates.shape[1]):
+    for i in range(3, rates.shape[1]-1):
         dct[f"T{i-2}"] = rates[:, i]
 
     rates_df = pd.DataFrame(dct)
@@ -131,9 +131,9 @@ def compare_ground(file_1, file_2, max_n):
     df_2 = rates_dataframe(file_2)
     df_2 = df_2.loc[(df_2["final"]==1.0) & (df_2["initial"] <= max_n)]
     
-    r_1 = df_1.values[:,2:]
-    r_2 = df_2.values[:,2:]
-
+    r_1 = df_1.values[:,7:]
+    r_2 = df_2.values[:,7:]
+    
     r_1[r_1==0] = 1e-99
     r_2[r_2==0] = 1e-99
     
@@ -152,9 +152,5 @@ if __name__ == "__main__":
     file_1 = "isoelectronic/he-like/o6/adf04_2Jmaxnx_70"
     file_2 = "isoelectronic/he-like/o6/adf04_2Jmaxnx_80" 
     
-
     compare_ground(file_1, file_2, 4)
     
-    compare(file_1, file_2)
-
-
