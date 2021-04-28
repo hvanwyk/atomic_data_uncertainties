9a169aca2552e1516c08981c08b3adfc8843a0a5
Hans-Werner van Wyk <hanswernervanwyk@gmail.com>
2021-04-28 06:53:28 -0500

    modified interpolator initialization

--- src/interpolation.py
+++ src/interpolation.py
@@ -21,10 +21,45 @@ Created on Tue Apr 27 11:00:57 2021
 class Interpolator:
     """
     """
-    def __init__(self, domain, function):
+    def __init__(self, domain, function, n_outputs, output_labels=None, 
+                 resolution='auto', tol=1e-6):
         """
         Constructor
+        
+        Inputs:
+            
+            domain: (d, 2) array with the boundaries of the hyper-rectangle, 
+                i.e. 
+            
+                domain = [x1_min, x1_max]
+                         [x2_min, x2_max]
+                            :       :
+                         [xd_min, xd_max]
+    
+            function: mapping from domain to R^n used to evaluate the true 
+                function.
+            
+            n_outputs: int, number of outputs
+            
+            output_labels: str/int, list of n_output output labels
+            
+            interpolation_type: str, type of interpolant to use
+                'global-polynomial', 'local-polynomial', 'wavelet'
+                
+            interpolation_degree: int, 
+            
+            tensorization:  
+                        
+            resolution: int/str, mesh resolution for the interpolator
+                
         """
+        # Dimension of hypercube
+        d = domain.shape[0]
+        
+        
+        if type(resolution) is int:
+            # Uniform resolution in all directions. 
+            p = Tasmanian.makeLocalPolynomialGrid()
         pass
     
     def add_labels(self, inputs=None, outputs=None):
@@ -48,7 +83,7 @@ class Interpolator:
         pass
     
     
-    def eval(self):
+    def evaluate(self):
         """
         """
         pass
