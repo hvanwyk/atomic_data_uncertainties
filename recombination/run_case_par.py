bd550f695af387a0cf79f570d08e4ae4a8ce2644
stuartloch <loch@physics.auburn.edu>
2021-10-24 00:53:52 -0500

    Updates to code to include 2-2 core excitation with capture into n=2 file

--- recombination/run_case_par.py
+++ recombination/run_case_par.py
@@ -17,7 +17,7 @@ from bayesian_methods import lambdas_grid
 from lambda_variation import energy_grid, rates_grid,  lambda_distribution, energy_distribution, rates_distribution, energy_optimization
 from utilities import get_nist_energy
 
-def run_case_par(seq,shell,nist_cutoff, prior_shape,likelihood_shape, cent_pot,n_walkers,n_steps,up_dir,nist_shift,grid_resolution,x_bnd,atom):
+def run_case_par(seq,shell,nist_cutoff, prior_shape,likelihood_shape, cent_pot,n_walkers,n_steps,up_dir,nist_shift,grid_resolution,x_bnd,egy_dist,atom):
     
     ion = State(atom, seq, shell)
     nist_vals = get_nist_energy(up_dir + f"/NIST/isoelectronic/{ion.isoelec_seq}/{ion.species}{ion.ion_charge}.nist")
@@ -30,13 +30,20 @@ def run_case_par(seq,shell,nist_cutoff, prior_shape,likelihood_shape, cent_pot,n
     lambda_samples, Err, Erg, err_interpolators,y_nist,accept,autocorrel = lambda_distribution(ion,up_dir,cent_pot=cent_pot,x_bnd=x_bnd, x_res=x_res, nist_cutoff=nist_cutoff, 
                       prior_shape=prior_shape, n_walkers=n_walkers, n_steps=n_steps,
                       likelihood_shape=likelihood_shape,emax=emax)
-
-    print('Calculating Energy distribution functions for',atom)
-    E_samples, Err, Erg=energy_distribution(ion, up_dir,emax, lambda_samples, x_bnd, x_res, cent_pot)
-
+    if (egy_dist == 1):
+       print('Calculating Energy distribution functions for',atom)
+       E_samples, Err, Erg=energy_distribution(ion, up_dir,emax, lambda_samples, x_bnd, x_res, cent_pot)
+    else:
+        E_samples='Option for energy distribution function was turned off, run again with egy_dist =0'
+        Err='Option for energy distribution function was turned off, run again with egy_dist =0'
+        Erg='Option for energy distribution function was turned off, run again with egy_dist =0'
+        
     print('Calculating rate distributions for',atom,' using cent_pot=',cent_pot,'emax=',emax,'nist_shift=',nist_shift)
     T, rate_samples,rates = rates_distribution(ion, up_dir,emax, lambda_samples, x_bnd, x_res, cent_pot,nist_shift=nist_shift)
     
     print('Finished',seq,'-like',atom)
-    result=[T,nist_vals,Erg,X_1D,lambda_samples,E_samples,Err,accept,autocorrel,rate_samples,rates]
+
+
+
+    result=[T,nist_vals,emax,Erg,x_res,X_1D,lambda_samples,E_samples,Err,accept,autocorrel,rate_samples,rates]
     return result
